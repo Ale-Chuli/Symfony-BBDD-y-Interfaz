e866e82879b604541dab8e326ab5cfc09cfc48c7
Ale-Chuli <ale.chulilla.vilar@gmail.com>
2025-01-19 20:32:47 +0100

    redirect

--- src/Controller/EmpresaController.php
+++ src/Controller/EmpresaController.php
@@ -10,10 +10,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EmpresaController extends AbstractController
 {
+    
+    #[Route('/', name: 'app_home')]
+    public function home(): RedirectResponse
+    {
+        return $this->redirectToRoute('app_inicio'); 
+    }
+
     #[Route('/inicio', name: 'app_inicio')]
     public function inicio(Request $request): Response
     {
